70bcec8f07c52e56c31d8ded3638810f113b329a
Nicholas Braaten <28822845+nicbra@users.noreply.github.com>
2023-07-14 01:04:35 +0200

    Are now able to solve sudoku

--- src/main.c
+++ src/main.c
@@ -1,10 +1,97 @@
 #include <stdio.h>
 #include <string.h>  // for memset
+#include <time.h>
 
 #include "sudokuHeader.h"
 #include "sudokuUtils.h"
 
+void fillUnambigousVals(struct Board *b) {
+    /*
+     * First we will find all unambigous values.
+     * I.e. values that can't be places anywhere else.
+     */
+    bool newRound = false;
+    do {
+        newRound = false;
+        for (size_t cell = 0; cell < 9 * 9; cell++) {
+            for (size_t val = 1; val < 10; val++) {
+                int unambPos = findUnambPos(ROW, cell, b, val);
+                if (unambPos != NO_POS) {
+                    b->boardValues[unambPos] = val;
+                    newRound = true;
+                }
+                unambPos = findUnambPos(COLUMN, cell, b, val);
+                if (unambPos != NO_POS) {
+                    b->boardValues[unambPos] = val;
+                    newRound = true;
+                }
+            }
+        }
+    } while (newRound);
+    return;
+}
+
+bool DFS_method(struct Board *b, int depth) {
+    // Copy to a new board to preserve the original
+    struct Board *b_copy = malloc(sizeof(*b));
+    memcpy(b_copy, b, sizeof(*b));
+
+    bool tryNextValue = false;
+
+    int cell = 0;
+    for (; cell < 9 * 9; cell++) {
+        if (b_copy->boardValues[cell] == 0) {
+            tryNextValue = true;
+            break;
+        }
+    }
+
+    int tryVal = 1;
+    int nRet;
+    if (tryNextValue) {
+    tryvalue_label:
+        memcpy(b_copy, b, sizeof(*b));
+        do {
+            nRet = tryCellValue(b_copy, cell, tryVal);
+            if (nRet == NO_RULE) {
+                b_copy->boardValues[cell] = tryVal;
+                // fillUnambigousVals(b_copy);
+            }
+            if (tryVal < 9) {
+                tryVal++;
+            } else {
+                break;
+            }
+
+        } while ((tryVal <= 9) && nRet != NO_RULE);
+    } else {
+        free(b_copy);
+        return true;
+    }
+
+    if (nRet == NO_RULE) {
+        if (!DFS_method(b_copy, depth + 1)) {
+
+            if (tryVal >= 9) {
+                free(b_copy);
+                return false;
+            } else {
+                goto tryvalue_label;
+            }
+        } else {
+            memcpy(b, b_copy, sizeof(*b));
+            free(b_copy);
+            return true;
+        }
+    } else {
+        free(b_copy);
+        return false;
+    }
+}
+
 int main() {
+    clock_t start_t, end_t;
+    start_t = clock();
     struct Board *board = malloc(sizeof(*board));
 
     // Initialize the struct
@@ -29,42 +116,18 @@ int main() {
         }
     }
 
-    printBoard(board);
 
-    /*
-     * First we will find all unambigous values.
-     * I.e. values that can't be places anywhere else.
-     */
-    bool newRound = false;
-    do {
-        printf("--\n");
-        newRound = false;
-        for (size_t cell = 0; cell < 9 * 9; cell++) {
-            for (size_t val = 1; val < 10; val++) {
-                int unambPos = findUnambPos(ROW, cell, board, val);
-                if (unambPos != NO_POS) {
-                    printf("Val %ld at pos %d\n", val, unambPos);
-                    board->boardValues[unambPos] = val;
-                    newRound = true;
-                }
-                unambPos = findUnambPos(BLOCK, cell, board, val);
-                if (unambPos != NO_POS) {
-                    printf("Val %ld at pos %d\n", val, unambPos);
-                    board->boardValues[unambPos] = val;
-                    newRound = true;
-                }
-                unambPos = findUnambPos(COLUMN, cell, board, val);
-                if (unambPos != NO_POS) {
-                    printf("Val %ld at pos %d\n", val, unambPos);
-                    board->boardValues[unambPos] = val;
-                    newRound = true;
-                }
-            }
-        }
-    } while (newRound);
+    fillUnambigousVals(board);
+
+    DFS_method(board, 0);
 
     printf("\n\n");
     printBoard(board);
 
     free(board);
+
+    end_t = clock();
+
+    double total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+    printf("Total ms taken by CPU: %f\n", total_t*1000  );
 }
